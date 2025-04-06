a93968b8f718e00395cd7c1da82c716e4ad53274
Victor Owino <vo4685336@gmail.com>
2025-04-06 10:14:50 +0300

    Refactor URL patterns to include pages app and add debug toolbar support

--- service_app/urls.py
+++ service_app/urls.py
@@ -14,24 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/orders/', include('orders.urls')),
-    path('api/services/', include('services.urls')),
-    path('api/users/', include('users.urls')),
-    path('api/payments/', include('payments.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("admin/", admin.site.urls),
+    path("", include("pages.urls")),
+    path("orders/", include("orders.urls")),
+    path("services/", include("services.urls")),
+    path("users/", include("users.urls")),
+    path("payments/", include("payments.urls")),
 ]
 
 if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
