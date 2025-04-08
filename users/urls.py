ff625a7381940f69111d36e35b8e20a679158e66
Victor Owino <vo4685336@gmail.com>
2025-04-08 12:59:44 +0300

    Add profile creation path to users URLs and standardize string quotes

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = 'users'
+app_name = "users"
 
 urlpatterns = [
-    path('register/', views.RegisterView.as_view(), name='register'),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('profile/', views.UserProfileView.as_view(), name='profile'),
+    path("register/", views.RegisterView.as_view(), name="register"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("profile/", views.UserProfileView.as_view(), name="profile"),
+    path("profile-create/", views.ProfileCreateView.as_view(), name="profile-create"),
 ]
